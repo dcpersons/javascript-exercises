bb30922f6970ba3f834ef0c19b02bac1e783711c
dcpersons <danielcpersons@gmail.com>
2026-06-08 17:49:14 -0400

    Complete javascript foundations exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item1, item2, item3, item4) {
+    for (let i = 0 ; i <= arr.length -1 ; i++) { 
+        if (arr[i] === item1 || arr[i] === item2 || arr[i] === item3 || arr[i] === item4) {
+            arr.splice(i, 1);
+            i--;
+        }
+    } return arr;
 };
 
 // Do not edit below this line
