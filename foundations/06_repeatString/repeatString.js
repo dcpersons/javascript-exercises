804d76ae3b8697db645e81910c54814c78f69ba7
dcpersons <danielcpersons@gmail.com>
2026-06-08 17:10:09 -0400

    Complete javascript foundations exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+// const repeatString = function(str, amount) {
+function repeatString(str, amount) {
+    let result = "";
+    if (amount < 0) {
+        return "ERROR"
+    } 
+    for (i = 0 ; i < amount ; i++) {
+        result += str
+    }
+    return result;
 };
 
 // Do not edit below this line
