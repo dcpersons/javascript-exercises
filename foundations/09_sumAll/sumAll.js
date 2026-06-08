de0bb1778f4e55c3f59a5128150befdfd6d23b74
dcpersons <danielcpersons@gmail.com>
2026-06-08 18:44:14 -0400

    Complete javascript foundations exercise 09
    Proud of this one ngl

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } let sum = 0;
+    let arr = [a, b];
+    arr = arr.sort((a, b) => a - b);
+    for (let i = arr[0] ; i <= arr[1] ; i++){
+        sum += i
+    } return sum;
 };
 
 // Do not edit below this line
