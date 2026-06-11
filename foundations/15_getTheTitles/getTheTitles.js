fcbe47ea47c7ae62b8c44c45f2bdbe67927a938a
dcpersons <danielcpersons@gmail.com>
2026-06-11 17:50:31 -0400

    complete javascript foundations exercise 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const titles = [];
+    arr.forEach((item) => {
+        titles.push(item.title);
+    })
+    return titles;
 };
 
 // Do not edit below this line
