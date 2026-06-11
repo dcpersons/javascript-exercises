4293b1514e0d1933fb8e1144fb0bc53c54b8d7bd
dcpersons <danielcpersons@gmail.com>
2026-06-11 17:33:12 -0400

    Complete javascript foundations exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (+n == 0) return 0;
+    if (!Number.isInteger(+n) || n < 0) return "OOPS";
+    let fib = 1;
+    let bo = 0;
+    let fibo = 1;
+    for (let i = 0 ; i <= +n-2 ; i++) {
+        fibo = fib + bo;
+        bo = fib;
+        fib = fibo;
+    } return fibo;
+// take number
+// add number to previous number
+// 
 };
 
 // Do not edit below this line
