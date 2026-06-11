b0925c24753036101eb8cdfd929335996744ace8
dcpersons <danielcpersons@gmail.com>
2026-06-11 17:02:11 -0400

    Complete javascript foundations exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const textClean = string.replaceAll(/[^a-z|0-9]/gi, "").toLowerCase();
+    let textReverse = textClean.split('').reverse().join('');
+    return textReverse == textClean;
 };
 
 // Do not edit below this line
