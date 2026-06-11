82edc9de55e2bad5f8de8632854223ab1d5d7f1c
dcpersons <danielcpersons@gmail.com>
2026-06-11 18:56:37 -0400

    Complete javascript foundations exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let date = new Date().getFullYear();
+    arr.sort((a, b) => {
+        if (a.yearOfDeath == undefined) {
+            return (b.yearOfDeath - b.yearOfBirth) - (date - a.yearOfBirth)
+        } if (b.yearOfDeath == undefined) {
+            return (date - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+        } else return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    });
+    return arr[0];
 };
 
 // Do not edit below this line
